b99fc66b8ae59cd14988b2e4cad3e2eefe39f158
Neal Robison <nealjrobison@gmail.com>
2018-01-24 10:45:38 -0500

    added barcode scanner

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,25 +1,39 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
-/**
- * Generated class for the HomePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
-@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html',
+  templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+    private barcodeScanner: BarcodeScanner) {
+
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad HomePage');
+  barcodeData: any;
+  scanResult: any;
+
+  goToUpdate() {
+    this.navCtrl.push('page-update');
   }
 
+  goToFetch() {
+    this.navCtrl.push('page-fetch');
+  }
+
+  ScanBarcode() {
+    this.barcodeScanner.scan().then((barcodeData) => {
+      console.log("Scan Successful: " + barcodeData.text);
+      this.scanResult = barcodeData.text;
+      alert("Scan Successful: " + this.scanResult);
+    }, (err) => {
+      console.error("Scan Failure: " + err);
+      alert("Scan Failure: " + err);
+      return;
+    });
+  }
 }
